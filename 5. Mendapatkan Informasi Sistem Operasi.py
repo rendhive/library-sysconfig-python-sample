b47f5e46f4026bb5ce79e5603ebe8eacaf365b56
rendhive <rendhivecode@gmail.com>
2026-06-30 21:17:32 +0700

    [DOCT] Sample Library Python: 5. Mendapatkan Informasi Sistem Operasi.py

---
+++ 5. Mendapatkan Informasi Sistem Operasi.py
@@ -0,0 +1,6 @@
+import sysconfig
+
+platform = sysconfig.get_platform()
+print(f"Platform yang digunakan: {platform}")
+# Fungsi: Mengetahui platform sistem operasi yang sedang digunakan.
+# Kondisi: Ketika Anda ingin mengetahui di platform mana Python dijalankan.
