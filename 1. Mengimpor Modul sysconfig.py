c2cac2f91d157ae3d54a033276e1aad664a46bf7
rendhive <rendhivecode@gmail.com>
2026-06-30 21:16:23 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul sysconfig.py

---
+++ 1. Mengimpor Modul sysconfig.py
@@ -0,0 +1,5 @@
+import sysconfig
+
+print("Modul sysconfig berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk mendapatkan informasi tentang konfigurasi lingkungan Python.
+# Kondisi: Digunakan sebelum memanggil fungsi atau fitur dari modul sysconfig.
