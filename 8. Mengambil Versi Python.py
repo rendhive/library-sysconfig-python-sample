a15e912bbaab0f75789f4b6baa995473944259ad
rendhive <rendhivecode@gmail.com>
2026-06-30 21:18:22 +0700

    [DOCT] Sample Library Python: 8. Mengambil Versi Python.py

---
+++ 8. Mengambil Versi Python.py
@@ -0,0 +1,6 @@
+import sysconfig
+
+python_version = sysconfig.get_python_version()
+print(f"Versi Python: {python_version}")
+# Fungsi: Mendapatkan versi Python yang sedang digunakan.
+# Kondisi: Ketika Anda ingin mengetahui versi spesifik dari Python yang sedang dijalankan.
