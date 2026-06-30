177dc893d18b1be385ab7bb4fa1348ea5c031af8
rendhive <rendhivecode@gmail.com>
2026-06-30 21:19:12 +0700

    [DOCT] Sample Library Python: 11. Melihat Konfigurasi Kompilasi.py

---
+++ 11. Melihat Konfigurasi Kompilasi.py
@@ -0,0 +1,6 @@
+import sysconfig
+
+print("Informasi konfigurasi:")
+print(sysconfig.get_config_vars())
+# Fungsi: Menampilkan semua variabel yang digunakan saat konfigurasi Python.
+# Kondisi: Ketika Anda ingin menyelidiki lebih dalam tentang kompilasi Python.
