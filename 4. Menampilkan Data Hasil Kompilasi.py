5eaae62841e638a21bfacd8c01b69cb8b78cda38
rendhive <rendhivecode@gmail.com>
2026-06-30 21:17:16 +0700

    [DOCT] Sample Library Python: 4. Menampilkan Data Hasil Kompilasi.py

---
+++ 4. Menampilkan Data Hasil Kompilasi.py
@@ -0,0 +1,6 @@
+import sysconfig
+
+print("Data hasil kompilasi:")
+print(sysconfig.get_makefile())
+# Fungsi: Menampilkan informasi tentang hasil kompilasi Python.
+# Kondisi: Ketika Anda ingin mendapatkan detail tentang cara Python dikompilasi.
